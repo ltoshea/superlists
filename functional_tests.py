fcbb7c71d0c0b32ad15d5256b6f96c0eedc70461
ltoshea <ltoshea+github@gmail.com>
2019-01-25 04:03:31 -0800

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -33,13 +33,14 @@ class NewVisitorTest(unittest.TestCase):
         
         #When He hits enter, the page updated, and now the page lists
         # 1: Buy peacock feathers as an item in a to-do list
-        inputbox.send_keys(Keys.Enter)
+        inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         table = self.browser.find_element_by_id('id_list_table')
-        rows = self.browser.find_element_by_tag_name('tr')
+        rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1. Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New Item did not appear in table"
         )
         
         #There is still a text box inviting him to add another item
