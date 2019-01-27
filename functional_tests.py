a5eab12240ceb9e00260b31d6c11e09b99b0acab
ltoshea <ltoshea+github@gmail.com>
2019-01-27 10:20:26 -0800

    Redirect after POST, and show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -8,12 +8,12 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-    
+
     def tearDown(self):
         self.browser.quit()
 
     def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table') 
+        table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
@@ -32,24 +32,23 @@ class NewVisitorTest(unittest.TestCase):
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
-        
+
         #He types "Buy peacock feather" into a text box (For fly fishing)
         inputbox.send_keys('Buy peacock feathers')
-        
+
         #When He hits enter, the page updated, and now the page lists
         # 1: Buy peacock feathers as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        
         #There is still a text box inviting him to add another item
         #He enters "use feathers to make a fly"
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        
+
         #The page updates again, and now shows both items on the list
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
