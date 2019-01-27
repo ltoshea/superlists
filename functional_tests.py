32e93bf71a3c75867b3870d245665d8178355798
ltoshea <ltoshea+github@gmail.com>
2019-01-27 05:18:24 -0800

    removing duplication in functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table') 
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         #Barry checks out the new to-do list app homepage
         self.browser.get('http://localhost:8000')
@@ -35,10 +40,8 @@ class NewVisitorTest(unittest.TestCase):
         # 1: Buy peacock feathers as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
         
         #There is still a text box inviting him to add another item
         #He enters "use feathers to make a fly"
@@ -48,10 +51,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
         
         #The page updates again, and now shows both items on the list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         #Barry woners whether the site will remember his list. Then he sees the site had generated a unique URL for him
         #some tet explains this/
