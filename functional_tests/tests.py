398c6a3b80d41ec75a2505adc17793cd7c56f694
ltoshea <ltoshea+github@gmail.com>
2019-01-28 01:59:19 -0800

    Made functional_Tests an app, use LiveServerTestCase to cleanup after functional tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,11 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -19,7 +20,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         #Barry checks out the new to-do list app homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         #They notice the page title and header mention to-do lists
         self.assertIn ('To-Do', self.browser.title)
@@ -60,5 +61,3 @@ class NewVisitorTest(unittest.TestCase):
         #He visist that URL - the to-do list is still there
 
         #Happy he goes to sleep
-if __name__ == '__main__':
-    unittest.main()
